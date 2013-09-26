2bfa6aa63b89ca30ce51319d1284a6eff0bd8da8
uchida <uchida.atsushi.8@gmail.com>
2013-09-26 18:47:38 +0900

    fix style

--- index.js
+++ index.js
@@ -21,18 +21,25 @@ var C2JS;
 
 $(function () {
 
+    var $editor = $("#editor-gs");
+    var editorSize = { width: $editor.width(), height: $editor.height() };
     var editor_gs = CodeMirror.fromTextArea(document.getElementById("editor-gs"), {
         lineNumbers: true,
         indentUnit: 4,
         mode: "text/x-csrc"
     });
     editor_gs.setValue("#include <stdio.h>\n\nint main(int argc, char* argv[]) {\n    printf(\"hello, world!\\n\");\n    return 0;\n}");
+    editor_gs.setSize(editorSize.width, editorSize.height);
+
+    var fileName = "Program";
+
+    $("#file-name").text(fileName+".c");
 
     $("#compile").click(function(e){
         var src = editor_gs.getValue();
         var opt = '-m'; //TODO
         var $output = $('#editor-error');
-        $output.text('$ gcc program.c -o program');
+        $output.text('$ gcc '+fileName+'.c -o '+fileName);
         $output.append('<br>');
         C2JS.Compile(src, opt, function(res){
             if(res == null) {
@@ -41,7 +48,7 @@ $(function () {
             }
             if(res.error.length > 0) {
                 $output.append(C2JS.TerminalColor(res.error.replace(/\n/g,"<br>\n")
-                        .replace(/\/.*\.c/g,"program.c")
+                        .replace(/\/.*\.c/g,fileName+".c")
                         .replace(/\/.*\/(.*\.h)/g, "$1")
                         .replace(/(note:.*)$/gm,"<span class='text-info'>$1</span>")
                         .replace(/(warning:.*)$/gm,"<span class='text-warning'>$1</span>")
@@ -50,7 +57,7 @@ $(function () {
                 $output.append('<br>');
             }
             if(!res.error.match("error:")) {
-                $output.append('$ ./program');
+                $output.append('$ ./'+fileName);
                 $output.append('<br>');
                 var Module = {print:function(x){$output.append(x+"<br>");/*console.log(x);*/}};
                 try {
